0f59c8cd51b0b2799f37e8f12348ac572eb9d918
elham jafari <elhamjafari@MacBook-Pro.local>
2023-11-17 16:26:04 -0800

    Get users api added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function getUsers(){
+
+        $user = User::latest('points')->get();
+        return response()->json($user);
+    }
+}
