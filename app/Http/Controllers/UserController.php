7d4db9a2c711d8b8863e60bb500df89852e9adfa
elham jafari <elhamjafari@MacBook-Pro.local>
2023-11-20 16:30:50 -0800

    group users based on their scores include the average age of the users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -71,6 +71,26 @@ class UserController extends Controller
         return response()->json(['message' => 'User points updated successfully']);
     }
 
+    //group users by scores and avg. ages
+    public function groupUsersByScore()
+    {
+        $users = User::selectRaw('ROUND(AVG(age)) as average_age, points')
+            ->groupBy('points')
+            ->orderBy('points', 'desc')
+            ->get();
+
+        $groupedUsers = [];
+
+        foreach ($users as $user) {
+            $groupedUsers[$user->points] = [
+                'names' => User::where('points', $user->points)->pluck('name')->toArray(),
+                'average_age' => $user->average_age,
+            ];
+        }
+
+        return response()->json($groupedUsers);
+    }
+
 
 
 }
